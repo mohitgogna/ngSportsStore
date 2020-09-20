214e16cc47b6d80c07423314f2f849881aabecf5
320062495 <320062495@YY168039>
2020-09-20 21:38:58 +0530

    model, datsource files

---
+++ src/app/model/product.model.ts
@@ -0,0 +1,7 @@
+export class product {
+    
+    constructor(public id?: number, public name?: string, public category?: string,
+        public description?: string, public price?: number) {
+
+    }
+}
