896ed7812d86a81dfb6a556768399fe755aba7b9
320062495 <320062495@YY168039>
2020-10-02 23:02:48 +0530

    store module and model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template:`<div class="bg-success p-2 text-center text-white">
-  This is SportsStore
-</div>`,
+  template:"<store></store>",
   //templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'SportsStore';
